89d6a7a585b33f31d09853834bb6922df0e8eccb
Arturs Meisters <arturs.meisters@gmail.com>
2020-10-14 18:51:03 +0100

    bin/run command

---
+++ spec/integration_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe 'bin/run' do
+  let(:path) { File.expand_path('fixtures/multiple_entries.log', __dir__) }
+
+  it 'prints total visits and unique visits per page' do
+    output = %x`bin/run #{path}`
+
+    expected_output = <<-STR
+      [[\"/index\", 3], [\"/contacts\", 2], [\"/about/2\", 1]]
+      [[\"/contacts\", 2], [\"/about/2\", 1], [\"/index\", 1]]
+    STR
+    expected_output.gsub!(/^#{expected_output.scan(/^[ \t]*(?=\S)/).min}/, "")
+    expect(output).to eq(expected_output)
+  end
+end
