168f6993908c7b3ab2c55f11e2f338f1aa0a533f
Arturs Meisters <arturs.meisters@gmail.com>
2020-10-14 19:59:41 +0100

    better output headers

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe 'bin/run' do
     expected_output = <<-STR
 
       Page views
-      ---------------------
-      | Page      | Count |
-      |-------------------|
-      | /index    | 3     |
-      | /contacts | 2     |
-      | /about/2  | 1     |
-      ---------------------
+      ----------------------
+      | Page      | Visits |
+      |--------------------|
+      | /index    | 3      |
+      | /contacts | 2      |
+      | /about/2  | 1      |
+      ----------------------
 
       Unique page views
-      ---------------------
-      | Page      | Count |
-      |-------------------|
-      | /contacts | 2     |
-      | /about/2  | 1     |
-      | /index    | 1     |
-      ---------------------
+      ----------------------------
+      | Page      | Unique views |
+      |--------------------------|
+      | /contacts | 2            |
+      | /about/2  | 1            |
+      | /index    | 1            |
+      ----------------------------
     STR
     expected_output.gsub!(/^#{expected_output.scan(/^[ \t]*(?=\S)/).min}/, '')
     expect(output).to eq(expected_output)
