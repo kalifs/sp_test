91f622b8166609f73d53d3a1e69c3c8026053cd9
Arturs Meisters <arturs.meisters@gmail.com>
2020-10-14 19:46:06 +0100

    Run script with table printer
    * integration test fix

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -7,8 +7,24 @@ RSpec.describe 'bin/run' do
     output = %x`bin/run #{path}`
 
     expected_output = <<-STR
-      [[\"/index\", 3], [\"/contacts\", 2], [\"/about/2\", 1]]
-      [[\"/contacts\", 2], [\"/about/2\", 1], [\"/index\", 1]]
+
+      Page views
+      ---------------------
+      | Page      | Count |
+      |-------------------|
+      | /index    | 3     |
+      | /contacts | 2     |
+      | /about/2  | 1     |
+      ---------------------
+
+      Unique page views
+      ---------------------
+      | Page      | Count |
+      |-------------------|
+      | /contacts | 2     |
+      | /about/2  | 1     |
+      | /index    | 1     |
+      ---------------------
     STR
     expected_output.gsub!(/^#{expected_output.scan(/^[ \t]*(?=\S)/).min}/, "")
     expect(output).to eq(expected_output)
